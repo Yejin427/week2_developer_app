1e5c6e0a0c9eca87149f59aebaddc6f6b195eb26
2020063436 <2020063436@hanyang.ac.kr>
2022-07-07 20:34:34 +0900

    intro commit

---
+++ app/src/main/java/com/example/week2_developer_app/IntroActivity.java
@@ -0,0 +1,31 @@
+package com.example.week2_developer_app;
+
+import android.media.MediaSession2;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+    private Button loginbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(IntroActivity.this)){
+                    login();
+                }
+                else{
+                    accountLogin();
+                }
+            }
+        });
+    }
+}
