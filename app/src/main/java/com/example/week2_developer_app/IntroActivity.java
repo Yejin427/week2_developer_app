6eae4bcd17b7781e78600869c91836c1caf5eb7a
sangyeob <rlatkdduq99@gmail.com>
2022-07-08 11:36:11 +0900

    login add

--- app/src/main/java/com/example/week2_developer_app/IntroActivity.java
+++ app/src/main/java/com/example/week2_developer_app/IntroActivity.java
@@ -1,28 +1,41 @@
 package com.example.week2_developer_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.kakao.auth.Session;
+import com.kakao.sdk.user.UserApi;
 import com.kakao.sdk.user.UserApiClient;
 import com.kakao.sdk.user.model.Account;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import retrofit2.http.Url;
+
 public class IntroActivity extends AppCompatActivity {
     private Button loginbtn;
+    ArrayList<String> userinfo = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-
         loginbtn = (Button)findViewById(R.id.button);
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(IntroActivity.this)){
                     login();
                 }
@@ -32,24 +45,26 @@ public class IntroActivity extends AppCompatActivity {
             }
         });
 
-
+        Log.d("str", userinfo.toString());
     }
 
     public void login(){
         String TAG = "login()";
+
         UserApiClient.getInstance().loginWithKakaoTalk(IntroActivity.this,(oAuthToken, error) -> {
             if (error != null) {
                 Log.e(TAG, "로그인 실패", error);
             } else if (oAuthToken != null) {
                 Log.i(TAG, "로그인 성공(토큰) : " + oAuthToken.getAccessToken());
                 getUserInfo();
-
             }
             return null;
         });
     }
+
     public void accountLogin(){
         String TAG = "accountLogin()";
+        ArrayList<String> tmp = new ArrayList<String>();
         UserApiClient.getInstance().loginWithKakaoAccount(IntroActivity.this,(oAuthToken, error) -> {
             if (error != null) {
                 Log.e(TAG, "로그인 실패", error);
@@ -64,22 +79,21 @@ public class IntroActivity extends AppCompatActivity {
     public void getUserInfo(){
         String TAG = "getUserInfo()";
         UserApiClient.getInstance().me((user, meError) -> {
-            if (meError != null) {
-                Log.e(TAG, "사용자 정보 요청 실패", meError);
-            } else {
+            if (meError != null) {}
+            else {
                 System.out.println("로그인 완료");
                 Log.i(TAG, user.toString());
                 {
-                    Log.i(TAG, "사용자 정보 요청 성공" +
-                            "\n회원번호: "+user.getId() +
-                            "\n이메일: "+user.getKakaoAccount().getName());
+                    userinfo.add(user.getId().toString());
+                    userinfo.add(user.getKakaoAccount().getProfile().getNickname());
+                    userinfo.add(user.getKakaoAccount().getEmail());
+
+                    Log.d("str", userinfo.toString());
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
                 }
-                Account user1 = user.getKakaoAccount();
-                System.out.println("사용자 계정" + user1);
             }
             return null;
         });
     }
-
-
 }
