3fbdc8242bcacd7411b106ab3cf4cd422bf12d03
sangyeob <rlatkdduq99@gmail.com>
2022-07-09 20:21:29 +0900

    Revert "main"
    This reverts commit 25b6088e49dfcca631719210fc96c54127514090.

--- app/src/main/java/com/example/week2_developer_app/AdapterProject.java
+++ app/src/main/java/com/example/week2_developer_app/AdapterProject.java
@@ -1,54 +1,34 @@
 package com.example.week2_developer_app;
 
 import android.content.Context;
-import android.content.Intent;
-import android.location.GnssAntennaInfo;
-import android.media.Image;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Filter;
 import android.widget.Filterable;
-import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 
 public class AdapterProject extends RecyclerView.Adapter<AdapterProject.ViewHolder> implements OnItemClickListener, Filterable{
+
 
     Context context;
     private ArrayList<Project> projects_filtered = new ArrayList<Project>();
     private ArrayList<Project> projects = new ArrayList<Project>();
     private ArrayList<Project> projects_list = new ArrayList<Project>();
     private static OnItemClickListener listener;
-    int viewtype = 0;
 
     public AdapterProject(ArrayList<Project> myData){
         this.context = context;
         this.projects_filtered = myData;
         this.projects = myData;
         this.projects_list.addAll(myData);
-        this.viewtype = 0;
-    }
-
-    public void setViewtype(int type){
-        this.viewtype = type;
-    }
-
-    public int getViewtype(){
-        return this.viewtype;
     }
 
     @NonNull
@@ -90,6 +70,7 @@ public class AdapterProject extends RecyclerView.Adapter<AdapterProject.ViewHold
                 filterResults.values = projects_filtered;
                 return filterResults;
             }
+
             @Override
             protected void publishResults(CharSequence constraint, FilterResults results) {
                 projects.clear();
@@ -101,8 +82,7 @@ public class AdapterProject extends RecyclerView.Adapter<AdapterProject.ViewHold
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
-        holder.onBind(projects.get(position), viewtype);
+        holder.onBind(projects.get(position));
     }
 
     public void setFriendList(ArrayList<Project> list){
@@ -136,18 +116,11 @@ public class AdapterProject extends RecyclerView.Adapter<AdapterProject.ViewHold
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
-
         TextView title;
-        TextView project_id;
         TextView information;
-        ImageButton imagebtn;
-        TextView viewType;
 
         public ViewHolder(@NonNull View itemView,final OnItemClickListener listener) {
             super(itemView);
-            project_id = (TextView) itemView.findViewById(R.id.proj_id);
-            viewType = (TextView) itemView.findViewById(R.id.viewType);
-            imagebtn = (ImageButton) itemView.findViewById(R.id.imagebtn);
             title = (TextView) itemView.findViewById(R.id.title);
             information = (TextView) itemView.findViewById(R.id.information);
 
@@ -157,31 +130,23 @@ public class AdapterProject extends RecyclerView.Adapter<AdapterProject.ViewHold
                 {
                     int pos = getAdapterPosition();
                     if(pos != RecyclerView.NO_POSITION){
-                        //동작 호출 (onItemClick 함수 호출)
                         if(listener != null){
-                            listener.onItemClick(ViewHolder.this, v, pos);
+                            listener.onItemClick(ViewHolder.this, v,pos);
                         }
                     }
                 }
             });
         }
-
-        void onBind(Project project, int viewtype) {
-
-            if(viewtype == 1) {
-                imagebtn.setImageResource(R.drawable.icon_bin);
-                viewType.setText(Integer.toString(viewtype));
-            }
-            Log.d("str", ""+project.getproj_id());
-            project_id.setText(Integer.toString(project.getproj_id()));
+        void onBind(Project project) {
             title.setText(project.gettitle());
+
             String str = "분야 : " + project.getfield()
                     + " / 언어 : " + project.getlanguage()
                     + " / 예상수준 : " + project.getlevel()
                     + " / 모집인원 : " + project.getheadcount() ;
+
             information.setText(str);
         }
-
     }
     public void setItems(ArrayList<Project> projects){
         this.projects = projects;
@@ -193,4 +158,3 @@ public class AdapterProject extends RecyclerView.Adapter<AdapterProject.ViewHold
         projects.set(position, project);
     }
 }
-
--- app/src/main/java/com/example/week2_developer_app/Project.java
+++ app/src/main/java/com/example/week2_developer_app/Project.java
@@ -6,17 +6,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public class Project {
-<<<<<<< Updated upstream
     private int id;
-=======
-    @SerializedName("proj_id")
-    private int proj_id;
-    @SerializedName("writer")
-    private String writer;
-    @SerializedName("writer_email")
-    private String writer_email;
-    @SerializedName("title")
->>>>>>> Stashed changes
     private String title;
     private String content;
     private String field;
@@ -25,15 +15,8 @@ public class Project {
     private String language; // 개발언어
     private String time;    // 선호시간
 
-<<<<<<< Updated upstream
     public Project(int id, String title, String content, String field, int level, int headcount, String language, String time){
         this.id = id;
-=======
-    public Project(int proj_id, String writer, String writer_email, String title, String content, String field, int level, int headcount, String language, String time, String regdata){
-        this.proj_id = proj_id;
-        this.writer = writer;
-        this.writer_email = writer_email;
->>>>>>> Stashed changes
         this.title = title;
         this.content = content;
         this.field = field;
@@ -43,8 +26,8 @@ public class Project {
         this.time = time;
     }
 
-    public int getproj_id() {
-        return proj_id;
+    public int getid() {
+        return id;
     }
     public String gettitle() {
         return title;
@@ -74,38 +57,4 @@ public class Project {
 interface GetProject{
     @GET("/user/getproject")
     Call<List<Project>> getProject();
-<<<<<<< Updated upstream
-=======
-    @POST("/user/getmyproject")
-    Call<List<Project>> getmyProject(@Body Project_myData project);
-    @POST("/user/addproject")
-    Call<ProjectResponse> addProject(@Body Project project);
-    @POST("/user/deleteproject")
-    Call<ProjectResponse> deleteProject(@Body Project_deleteData project);
-}
-
-class ProjectResponse {
-
-    @SerializedName("code")
-    private int code;
-    public int getCode() {
-        return code;
-    }
-}
-
-class Project_myData{
-    @SerializedName("userEmail")
-    String userEmail;
-    public Project_myData(String userEmail){
-        this.userEmail = userEmail;
-    }
-}
-
-class Project_deleteData{
-    @SerializedName("proj_id")
-    int proj_id;
-    public Project_deleteData(int proj_id){
-        this.proj_id = proj_id;
-    }
->>>>>>> Stashed changes
 }
